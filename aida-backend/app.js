26f4a4edcaaa58e9de96561cc3156944704ab52e
Saikat Ray <Saikat.Ray@hyland.com>
2022-08-23 00:07:42 +0530

    load the api

---
+++ aida-backend/app.js
@@ -0,0 +1,137 @@
+'use strict'
+
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectID;
+const q = require('q');
+const EventEmitter = require('events')
+
+const app = express()
+const eventEmitter = new EventEmitter();
+
+let config   =   require('./package.json').config;
+
+let mongoClient = null;
+let mongoDatabase = null;
+
+let mongoCollection = null;
+
+function readCache(req, res) {
+
+    if (!mongoClient.isConnected()) {
+        res.status(504);
+        res.send('MongoDB not connected');
+        return;
+    }
+
+    if (!req.params || !req.params.cacheId) {
+        res.status(400);
+        res.send('cacheId not found');
+        return;
+    }
+
+    mongoCollection.find({'_id': ObjectId(req.params.cacheId)}).toArray(function(err, docs) {
+        if (!err) {
+            if (docs && docs.length > 0) {
+                res.send(docs[0])
+            } else {
+                res.status(404);
+                res.send('cacheId not found');
+            }
+        } else {
+            res.status(500);
+            res.send(err.message);
+        }
+      });
+}
+
+function writeCache(req, res) {
+
+    if (!mongoClient.isConnected()) {
+          res.status(504);
+          res.send('MongoDB not connected');
+          return;
+    }
+
+    if (!req.body) {
+        res.status(400);
+        res.send('missing body');
+        return;
+    }
+    
+    mongoCollection.insertOne(req.body, function(err, result) {
+        if (!err) {
+            res.json({id: result.ops[0]._id});
+        } else {
+            res.status(500);
+            res.send(err.message);
+        }
+      });
+}
+
+function attachHandlers() {
+    
+    app.use(express.json())
+
+    return q();
+}
+
+function createRoutes() {
+
+    app.get('/api/health', (req, res) => res.send(''));
+    app.get('/api/cache/:cacheId', readCache);
+    app.post('/api/cache', writeCache);
+
+    return q();
+}
+
+
+
+
+function serve() {
+    
+    app.listen(config.service.port, () => console.log(`cinemo-internal listening on port ${config.service.port}`));
+    
+    return q();
+}
+
+function connectMongoDb() {
+
+    let deferred = q.defer();
+
+    mongoClient = new MongoClient(config.mongodb.url);
+
+    mongoClient
+        .connect(function(err) {
+            if (!err) {
+                console.log(`Successfully connected to MongoDB at '${config.mongodb.url}'`);
+                mongoDatabase = mongoClient.db(config.mongodb.databaseName);
+                mongoCollection = mongoDatabase.collection(config.mongodb.collectionName);
+                deferred.resolve();
+            } else {
+                console.log(`Failed to connect to MongoDB at '${config.mongodb.url}'`);
+                deferred.reject(err);
+            }
+        });
+
+    return deferred.promise;
+}
+
+function main() {
+    
+    connectMongoDb()
+        .then(attachHandlers)
+        .then(createRoutes)
+        .then(serve)
+        .catch(function(err) {
+            console.log(err);
+            eventEmitter.emit('done');
+        });
+
+    // keep alive                           
+    eventEmitter.on('done', function(r) {});
+}
+
+(function() {
+      main();
+})();
---
+++ aida-frontend/app.js
@@ -0,0 +1,193 @@
+'use strict'
+
+const express = require('express');
+const q = require('q');
+const axios = require('axios');
+const EventEmitter = require('events')
+const aws = require('aws-sdk');
+
+const app = express()
+const eventEmitter = new EventEmitter();
+const TraceHeader = 'x-trace';
+
+let config = require('./package.json').config;
+let kinesis = null;
+
+function readCustomer(req, res) {
+
+    writeMetric('readCustomer.attempt');
+
+    let traceId = req.headers[TraceHeader] || '';
+
+    if (!req.params || !req.params.customerId) {
+        writeMetric('readCustomer.fail');
+        writeLog(`No customerId specified`, traceId);
+        res.status(400);
+        res.send('customerId missing');
+        return;
+    }
+
+    writeLog(`Reading customer ${req.params.customerId}`, traceId);
+
+    axios
+        .get(config.dependencies.cinemoInternalUrl + '/api/cache/' + req.params.customerId)
+        .then((response) => {
+            writeMetric('readCustomer.success');
+            writeLog(`Successfully read customer with customerId '${req.params.customerId}'`, traceId);
+            res.json(response.data);
+        })
+        .catch(error => {
+            writeMetric('readCustomer.fail');
+            writeLog(`Failed to read customer with customerId '${req.params.customerId}'`, traceId);
+            writeLog(error.response.data, traceId);
+            res.status(error.response.status);
+            res.send(error.response.data);
+        });   
+}
+
+function CreateCustomer(req, res) {
+    
+    writeMetric('createCustomer.attempt');
+
+    let traceId = req.headers[TraceHeader] || '';
+
+    if (!req.body || !req.body.name) {
+        writeLog(`No customer name specified`, traceId);
+        writeMetric('createCustomer.fail');
+        res.status(400);
+        res.send('customer name missing');
+        return;
+    }
+
+    writeLog(`Creaing new customer with name ${req.body.name}`, traceId);
+
+    let customer = {
+        name: req.body.name,
+        dateCreated: new Date().getTime()
+    };
+
+    axios
+        .post(config.dependencies.cinemoInternalUrl + '/api/cache', customer)
+        .then((response) => {
+            writeMetric('createCustomer.success');
+            writeLog(`Successfully created customer with name ${req.body.name}`, traceId);
+            res.json({customerId: response.data.id});
+        })
+        .catch(error => {
+            writeMetric('createCustomer.fail');
+            writeLog(`Failed to create customer with name ${req.body.name}`, traceId);
+            writeLog(error.response.data, traceId);
+            res.status(error.response.status);
+            res.send(error.response.data);
+        });
+}
+
+function writeLog(message, traceId) {
+
+    if (!traceId) {
+        return;
+    }
+
+    let frame = {
+        type: 'logs',
+        data: {
+            appId: 'cinemo-public',
+            traceId: traceId,
+            message: message,
+            timestamp: new Date().getTime()
+        }
+    };
+
+    let record = {
+        Data: JSON.stringify(frame),
+        StreamName: config.kinesis.streamName,
+        PartitionKey: config.kinesis.partitionKey
+      };
+
+    kinesis
+        .putRecord(record, function(err, data) {
+            if (err) {
+                console.error(err);
+            }
+        });
+}
+
+function writeMetric(name) {
+
+    let frame = {
+        type: 'metrics',
+        data: {
+            appId: 'cinemo-public',
+            name: name,
+            count: 1,
+            timestamp: new Date().getTime()
+        }
+    };
+
+    let record = {
+        Data: JSON.stringify(frame),
+        StreamName: config.kinesis.streamName,
+        PartitionKey: config.kinesis.partitionKey
+      };
+
+    kinesis
+        .putRecord(record, function(err, data) {
+            if (err) {
+                console.error(err);
+            }
+        });
+}
+
+function connectKinesis() {
+
+    aws.config.loadFromPath('./aws.json');
+                                          
+    kinesis = new aws.Kinesis({
+        region: config.kinesis.region,
+        endpoint: config.kinesis.endpoint,
+    });
+
+    return q();
+}
+
+function attachHandlers() {
+    
+    app.use(express.json())
+
+    return q();
+}
+
+function createRoutes() {
+
+    app.get('/api/health', (req, res) => res.send(''));
+    app.get('/api/customers/:customerId', readCustomer);
+    app.post('/api/customers', CreateCustomer);
+
+    return q();
+}
+
+function serve() {
+    
+    app.listen(config.service.port, () => console.log(`cinemo-public listening on port ${config.service.port}`));
+    
+    return q();
+}
+
+function main() {
+
+    connectKinesis()
+        .then(attachHandlers)
+        .then(createRoutes)
+        .then(serve)
+        .catch(function(err) {
+            console.log(err);
+            eventEmitter.emit('done');
+        });
+
+    // keep alive                           
+    eventEmitter.on('done', function(r) {});
+}
+
+(function() {
+      main();
+})();
