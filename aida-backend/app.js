e756bb73d25258ea018130f6755ce9a2786ecd0f
Saikat Ray <Saikat.Ray@hyland.com>
2022-08-25 20:05:49 +0530

    changes for docker

--- aida-backend/app.js
+++ aida-backend/app.js
@@ -90,7 +90,7 @@ function createRoutes() {
 
 function serve() {
     
-    app.listen(config.service.port, () => console.log(`cinemo-internal listening on port ${config.service.port}`));
+    app.listen(config.service.port, () => console.log(`aida-internal listening on port ${config.service.port}`));
     
     return q();
 }
